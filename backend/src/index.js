28e5295aef98648874300b2b13e83cfc86fc5f83
Yogesh kharb <115645162+Yogeshkharb111@users.noreply.github.com>
2025-07-26 11:28:18 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+/*
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -37,6 +37,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+*/
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
